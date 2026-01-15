20190b50ea64c59876e147cd9a1d40dcb8c13255
mdsalikneyazi <mdsalikneyazi24539@gmail.com>
2026-01-15 22:42:21 +0530

    Cleaned package.json for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,19 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // 👈 REQUIRED for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Points to your Backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 })
